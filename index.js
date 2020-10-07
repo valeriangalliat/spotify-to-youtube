d9629ebb8931ef1644b8443772fe4dba4157c5f1
Val <val@codejam.info>
2020-10-07 07:49:38 -0400

    Change algorithm to prefer songs and allow retrieving raw track

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ function SpotifyToYoutube (spotifyApi) {
   const youtubeApi = new YoutubeMusicApi()
   let youtubeInitialized = false
 
-  return async function spotifyToYoutube (urisOrIdsOrTracks) {
+  return async function spotifyToYoutube (urisOrIdsOrTracks, options = {}) {
     if (!Array.isArray(urisOrIdsOrTracks)) {
-      return (await spotifyToYoutube([urisOrIdsOrTracks]))[0]
+      return (await spotifyToYoutube([urisOrIdsOrTracks], options))[0]
+    }
+
+    if (!options.raw) {
+      return (await spotifyToYoutube(urisOrIdsOrTracks, Object.assign({}, options, { raw: true }))).map(track => track.videoId)
     }
 
     if (typeof urisOrIdsOrTracks[0] === 'string') {
       const ids = urisOrIdsOrTracks.map(uriOrId => uriOrId.includes(':') ? uriOrId.split(':').pop() : uriOrId)
       const { tracks } = (await spotifyApi.getTracks(ids)).body
 
-      return spotifyToYoutube(tracks)
+      return spotifyToYoutube(tracks, options)
     }
 
     if (!youtubeInitialized) {
@@ -24,20 +28,22 @@ function SpotifyToYoutube (spotifyApi) {
     return Promise.all(urisOrIdsOrTracks.map(async track => {
       const { content } = await youtubeApi.search(`${track.artists.map(artist => artist.name).join(' ')} ${track.name}`)
 
-      if (content[0].type === 'song') {
-        return content[0].videoId
-      } else if (content[1].type === 'song') {
-        return content[1].videoId
-      } else if (content[0].type === 'video') {
-        return content[0].videoId
+      const songsAndVideos = content.filter(track => track.type === 'song' || track.type === 'video')
+
+      if (songsAndVideos.length < 1) {
+        return null
+      }
+
+      if (songsAndVideos.length === 1) {
+        return songsAndVideos[0]
+      }
+
+      if (songsAndVideos[0].type === 'song') {
+        return songsAndVideos[0]
+      } else if (songsAndVideos[1].type === 'song') {
+        return songsAndVideos[1]
       } else {
-        const match = content.find(track => track.type === 'song' || track.type === 'video')
-
-        if (!match) {
-          return null
-        }
-
-        return match.videoId
+        return songsAndVideos[0]
       }
     }))
   }
